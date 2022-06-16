b3c4a6b4846eca90f6c8370ba1c70f36c8e935e9
CuetoMaryJoy <101856429+CuetoMaryJoy@users.noreply.github.com>
2022-06-16 15:41:56 +0800

    Update head.blade.php

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,10 +2,18 @@
 
 <meta name="description" content="">
 
-<meta name="Saquib" content="Blade">
+<meta name="Mary Joy"content="Blade">
 
-<title>Check our layout</title>
+<title>CUETO_Laravel_Finals_Activity</title>
 
-<!--load bootsrap from cdn-->
+<!-- load bootstrap from a cdn -->
 
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/twitter-bootstrap/3.0.3/css/bootstrap-combined.min.css">
+
+<!-- FONTS STYLE  -->
+<link href="https://fonts.googleapis.com/css2?family=Passion+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Francois+One&family=Passion+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Radio+Canada:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
